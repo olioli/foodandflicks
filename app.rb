3174d0ea50b649db0436aade6826ca67302b292c
mazesoul <mazesoul@gmail.com>
2010-11-27 13:02:55 -0500

    Added views

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'model'
 
 get '/' do
   "Yo dawg, I hear you like dinner and a movie so I put a dinner in your movie so you could dine while you movies."
+  haml :index, :format => :html5, :page_id => 'index'
 end
 
 #get '/create' do
