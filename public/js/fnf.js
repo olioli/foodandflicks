e00305aca84b209ac6ec237ecb653d96ec959e0d
mazesoul <mazesoul@gmail.com>
2010-11-27 15:42:43 -0500

    WIP

--- public/js/fnf.js
+++ public/js/fnf.js
@@ -1,63 +1,88 @@
 
-$(function(){
   
   // Load the flicks
-  var flicks    = null
-    , theaters  = null
-    , selection = {}
-    
-    , flk       = {};
+var flicks    = null
+  , theaters  = null
+  , selection = null
+  , selel     = null
+  , flk       = {};
   // Load the theaters
   
+
+function getFlicks( ){
+    $.getJSON( '/flicks'
+             , function( data ){
+                  
+                  showFlicks( data.filter(function(f){ return f.thumbnail; })
+                                 .slice(0,18) );    
+                } );
+}
+
+function showFlicks( flicks ){
+                   
+  flicks.forEach( function( flick ){
+                    console.debug(flick.title)
+                    $('<li>').addClass('flick')
+                             .appendTo('#flicks')
+                             .append('<img>')
+                             .find('img')
+                                .data('f', flick )
+                                .click( selectFlick )
+                                 .attr(  { src   : flick.thumbnail
+                                          , title : flick.title
+                                          } );
+                  } );
+
+}
+function selectFlick( e ){
+  showTheater( $(this).data('f')  );
+}
   
-  flk = { get       : function( ){
-                        console.log('get')
-                        $.getJSON( '/flicks'
-                                 , function( data ){
-                                      console.debug(data)
-                                      flicks = data;
-                                      flk.show();    
-                                    } );
-                      }
-        , show      : function(){
-          
-                  }
-        , select: function( e ){
-                    var el    = $(this)
-                      , flick = el.data('f')
-                    if( selection[flick.id] ){
-                      delete selection[flick.id]
-                      el.removeClass('highlight');
-                    }
-                    else{
-                       selection[flick.id] = flick;
-                       el.addClass('highlight');
-                    }
-                  }
-         , show : function(){
-                    console.debug(flicks)
-                    flicks.forEach( function( flick ){
-                                      console.debug(flick.title)
-                                      $('<li>').addClass('flick')
-                                               .appendTo('#flicks')
-                                               .click(flk.select)
-                                               .data('f', flick )
-                                               .append('<img>')
-                                               .find('img')
-                                                .attr(  { src   : flick.thumbnail
-                                                        , title : flick.title
-                                                        } );
-                                    } );
-      
-                  }
-  };
+function showTheater( flick ){
+  $.getJSON('/flicks/' + flick.id
+           , function( flick ){
+                $('#theater').html( flickHtml(flick) )
+                             .show(); 
+             });
   
   
+}
+  
+function flickHtml( flick){
+  var html = ''
+  
+  
+  return html;
+} 
+  
+function flickOk(){
+  // Update selection with time and theater
   
   
   
+}
+
+function getFood( ){
+  
+  $.getJSON('/food/' + selection.flick.theater )
+  
+}
+
+function showFood(){
+  
+  
+  
+}
   // Run app
   
-  flk.get();
+$(  getFlicks );
   
-});
+  // Pick
+  // Theater
+  // Food
+  // Time
+  // Poll using twitter
+  // Post to soiree
+  
+  
+
