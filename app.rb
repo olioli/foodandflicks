a91f0ba81070fb7440aef7c70fadc5da1f41f04e
bruno-c <neonskimmer@gmail.com>
2010-11-27 13:26:23 -0500

    doing stuff

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
-require 'base64'
 require 'sinatra'
 require 'model'
+require 'rest_client'
 
 get '/' do
   "Yo dawg, I hear you like dinner and a movie so I put a dinner in your movie so you could dine while you movies."
