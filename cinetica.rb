c637b7e299d6631b19dd7cef3795577be916e0d1
bruno-c <neonskimmer@gmail.com>
2010-11-27 15:26:00 -0500

    food

--- cinetica.rb
+++ cinetica.rb
@@ -1,7 +1,18 @@
 CINETICA_URL = "http://api.cineti.ca"
 
 get '/flicks' do
+
+  # TODO: WHERE'S THE ID?  at the end of href attributes
+  
   content_type :json
   @api = RestClient::Resource.new CINETICA_URL
-  body @api['/movies.json'].get.body
+  body @api["/movies.json"].get.body
+end
+
+
+get '/flicks/:id' do
+  content_type :json
+  dayparam = params[:day] || "sat"
+  @api = RestClient::Resource.new CINETICA_URL
+  body @api["/movies/#{params[:id]}.json?day=#{dayparam}"].get.body
 end
--- yellow.rb
+++ yellow.rb
@@ -1,6 +1,25 @@
 
-get '/food' do
-  content_type :json
+YELLOW_URL = "http://api.yellowapi.com"
+YELLOW_SB_URL = "http://api.sandbox.yellowapi.com"
+YELLOW_API_KEY = "g4hek5ph5za79bgh9b6eqc6p"
+
+RestClient.log = Logger.new(STDOUT)
+
+get '/food/:cinema' do
+  cinema = params[:cinema] || "amc"
+  @cinema = JSON.parse((RestClient::Resource.new(CINETICA_URL))["/theater/#{cinema}.json"].get)
+  #@cinemas = JSON.parse((RestClient::Resource.new(CINETICA_URL))["/theaters.json"].get)
+
+  lat, long = @cinema["lat"], @cinema["long"]
   
-    
+  @api = RestClient::Resource.new YELLOW_SB_URL
+  
+  food = @api['/FindBusiness/'].get( { :params => { :what => "restaurant", 
+                             :where => "cZ#{long},#{lat}", 
+                             :fmt => "JSON", 
+                             :pgLen => 50, 
+                             :UID => "31337",
+                             :apikey => YELLOW_API_KEY } })
+  content_type :json
+  body food.body
 end
