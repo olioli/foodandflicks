9c9c8ad4bd6d5f1a3259068426f2a7f9dd67e638
Mathieu Perreault <mathp@aircooled.local>
2010-11-27 14:30:20 -0500

    post stuff

--- app.rb
+++ app.rb
@@ -16,9 +16,27 @@ get '/soiree/:id' do
   current = Event.get(params[:id])
   if current == nil
     # Event not found, serve some page saying it doesn't exist
-    redirect '/soiree/create'
+    redirect '/'
   else 
-    current.jsonstring
+    "showing page with json string #{current.jsonstring}"
+  end
+end
+
+post '/soiree' do
+
+  begin
+    @b = request.body.read
+    puts @b.class
+    @parsed_object = JSON.parse(@b)
+    puts @parse_object.class
+    #newsoiree = Event.new
+    #@parsed_object['id'] = newsoiree.id
+    #newsoiree.jsonstring = @parsed_object.to_json
+    #newsoiree.save
+    #puts "create #{newsoiree.id} with id #{newsoiree.jsonstring}"
+  rescue Exception => e 
+    puts "there was a fuck up #{@b}"
+    e
   end
 end
 
